70981836deffefdff897dbb19ae021025a679bb8
Orly Claudio Gómez Ramírez <orly@MacBook-Pro-de-Orly.local>
2016-11-10 21:37:33 -0600

    Initial Commit

---
+++ ejemplo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ejemplo
+//
+//  Created by Orly Claudio Gómez Ramírez on 10/11/16.
+//  Copyright © 2016 Orly Claudio Gómez Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
